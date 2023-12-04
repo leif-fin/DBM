393199918199ab7498d0b5f3c0c5df207267524a
leif-fin <u6318412@au.edu>
2023-12-04 16:22:41 +0700

    First try - uploading code

---
+++ walletapp/app.py
@@ -0,0 +1,17 @@
+def test01():  #during function
+    a = 5
+    #print(s)
+
+def test02():
+    c1 = 3
+    c2 = 3
+    code = str(c1) + str(c2)
+    print(code)
+
+def test03():
+    s1 = '10'
+    s2 = '21'
+    sum = float(s1) + float(s2)
+    print(sum)
+
+
