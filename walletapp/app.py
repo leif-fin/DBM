85120985cd744eebf009523ef46dc5aa188490ab
leif-fin <u6318412@au.edu>
2023-12-12 16:21:21 +0700

    First try - uploading code

--- walletapp/app.py
+++ walletapp/app.py
@@ -14,4 +14,53 @@ def test03():
     sum = float(s1) + float(s2)
     print(sum)
 
+def selection02():
+    a = 300
+    b = 200
+    if b > a:
+        print("b is greater than a")
+    elif a == b:
+        print("a and b are equal")
+    else:
+        print("a is greater than b")
+
+def forloop01():
+    fruits = ["apple", 15, "banana","cherry", "potato", "tomato"]
+    for i in fruits:
+        print('There is the ' + str(i))
+
+def forloop02():
+    full_name = 'Assumption University'
+    print(len(full_name))
+
+    for i in full_name:
+       # if i != ''
+        print('The alphabet is ' + i)
+
+def whileloop01():
+    i = 1
+    sum = 0
+    while i < 6:
+        sum += i
+        i += 1
+    print(sum)
+
+def whileloop02():
+    # start at 10
+    # increase by 5
+    # end with 200
+    # 10 + 15 + 20 + ... + 200
+    i = 10
+    sum = 0
+    while i < 205:
+        sum += i
+        i += 5
+    print(sum)
+
+def whileloop3():
+    nums = range(10,205,5)
+    sum = 0
+    for i in nums:
+        sum += i
+    print(sum)
 
--- walletapp/main.py
+++ walletapp/main.py
@@ -1,6 +1,9 @@
-import walletapp.app as app
+import app as app
 
 if __name__ == '__main__':
-   app.test01()
-   app.test02()
-   app.test03()
+   #app.selection02()
+   #app.forloop01()
+   #app.forloop02()
+   #app.whileloop01()
+   #app.whileloop02()
+   app.whileloop3()
