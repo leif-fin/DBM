dc231c0c1389624838e7d912475524783a327d42
leif-fin <u6318412@au.edu>
2024-03-06 12:05:22 +0700

    app.py

---
+++ a01.py
@@ -0,0 +1,14 @@
+class Vinyl:
+    def __init__(self, album, artist, year):
+        self.album = album
+        self.artist = artist
+        self.year = year
+
+    def display(self):
+        print(self.album + ' ' + self.artist +' ' + self.year)
+
+def run01():
+    taylor = Vinyl('1989', 'Taylor Swift', '2014')
+    taylor.display()
+
+run01()
