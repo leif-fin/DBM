be87b894b015dd32b0cb246ce95f0f8c4efe47a1
leif-fin <u6318412@au.edu>
2024-03-07 22:15:33 +0700

    Add term project

---
+++ termProject/BST.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val < key:
+            root.right = insert(root.right, key)
+            print('right')
+            print(key)
+        else:
+            root.left = insert(root.left, key)
+            print('left')
+            print(key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if root.val < key:
+        return search(root.right, key)
+    return search(root.left, key)
+
+
+root = Node(50)
+insert(root, 30)
+insert(root, 20)
+insert(root, 40)
+insert(root, 70)
+insert(root, 60)
+insert(root, 80)
+insert(root, 10)
+
+# Search for a value
+if search(root, 60):
+    print("Found")
+else:
+    print("Not Found")
